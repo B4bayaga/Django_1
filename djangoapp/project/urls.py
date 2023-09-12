5a35b0349344aa8c1686e86cd57c1f0753b4a247
Rafael Oliveira <rafael@lyquidity.com.br>
2023-09-12 14:14:01 -0300

    Subindo página de entrada e criando app recipes.

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -18,9 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from recipes.views import my_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', my_view),
 ]
 
 if settings.DEBUG:
