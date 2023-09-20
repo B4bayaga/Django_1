5c1d0ff08213080fe0453cf1efb6942902a1c21b
Rafael Oliveira <rafael@lyquidity.com.br>
2023-09-20 15:48:25 -0300

    Atualizando.

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -17,13 +17,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from recipes.views import my_view
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view),
+    path('', include('recipes.urls')),
 ]
 
 if settings.DEBUG:
