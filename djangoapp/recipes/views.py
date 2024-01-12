a6629d6ccfad01b0a201b36031c43d46aceca5be
Rafael <rafael@manjaro.com>
2024-01-12 09:53:04 -0300

    Renderizando template home.htlm.

--- djangoapp/recipes/views.py
+++ djangoapp/recipes/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def my_view(request):
-    return HttpResponse('Começou a brincadeira!')
+    return render(request, 'recipes/home.html')
